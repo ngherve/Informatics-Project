f8fa1a6c75bd87afb24a1c2a47c0b5384b6f7bdd
Trust <trustnmpofu@gmail.com>
2019-04-22 21:07:55 +0200

    Added Database Api

---
+++ AWilec/AWilec/IWilecDBC.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace AWilec
+{
+   
+    [ServiceContract]
+    public interface IWilecDBC
+    {
+        [OperationContract]
+        bool Login(string un,string psw);
+
+        [OperationContract]
+        void Register( string FN, string surname, string uname, string password,  string Gender);
+
+        [OperationContract]
+        string AddProduct(string name, int price, string image, int quantity, string status);
+
+        [OperationContract]
+        string DeleteProduct(string Name);
+
+        [OperationContract]
+        string getProduct(string Name);
+
+        [OperationContract]
+        string UpdateProductPrice(string name,int Price);
+
+    }
+}
---
+++ AWilec/AWilec/wProduct.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace AWilec
+{
+    public class wProduct
+    {
+        private string name;
+        public string getName()
+        {
+            return name;
+        }
+        public void setName(string name)
+        {
+            this.name = name;
+        }
+        private int price;
+        public int getPrice()
+        {
+            return price;
+        }
+        public void setPrice(int price)
+        {
+            this.price = price;
+        }
+        private string image;
+        public string getImage()
+        {
+            return image;
+        }
+        public void setImage(string image)
+        {
+            this.image = image;
+        }
+        private int quantity;
+        public int getQuantity()
+        {
+            return quantity;
+        }
+        public void setQuantity(int quantity)
+        {
+            this.quantity = quantity;
+        }
+        private string stat;
+
+        public string getStatus()
+        {
+            return stat;
+        }
+        public void setStatus(string stat)
+        {
+            this.stat = stat;
+        }
+
+
+        public wProduct(string n,int p,string i,int q,string s)
+        {
+            name = n;
+            price = p;
+            image = i;
+            quantity = q;
+            stat = s;
+        }
+        
+        static public List<wProduct> getProducts()
+        {
+            List<wProduct> list = new List<wProduct>();
+
+            return list;
+        }
+    }
+}
