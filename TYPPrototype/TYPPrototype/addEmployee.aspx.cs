3e056e09aa5f09ba0af17295929fbdb75f5c60a9
ngherve <217092052@student.uj.ac.za>
2019-05-14 15:59:04 +0200

    Adding the web App

---
+++ TYPPrototype/TYPPrototype/addEmployee.aspx.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+
+namespace TYPPrototype
+{
+    public partial class addEmployee : System.Web.UI.Page
+    {
+        UserService.UserServiceClient client;
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            client = new UserService.UserServiceClient();
+        }
+
+        protected void btnSaveEmpl_Click(object sender, EventArgs e)
+        {
+            
+            UserService.User user = new UserService.User
+            {
+                Name = fname.Value + " " + lname.Value,
+                Username = username.Value,
+                Email = email.Value,
+                Password = pass.Value,
+                Tel_Number = telnum.Value,
+                Address = address.Value,
+                Gender = gender.Value,
+                DOB = dob.Value
+            };
+            string result = client.RegisterUser(user);
+
+            Response.Write("<script>alert('" + result + "')</script>");
+        }
+    }
+}
---
+++ TYPPrototype/TYPPrototype/addProduct.aspx
@@ -0,0 +1,5 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/MasterPage.Master" AutoEventWireup="true" CodeBehind="addProduct.aspx.cs" Inherits="TYPPrototype.addProduct" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="head" runat="server">
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" runat="server">
+</asp:Content>
