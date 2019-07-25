eaf5b3640d86df8c6da470e08701290c8ccc35d6
Trust <trustnmpofu@gmail.com>
2019-07-26 01:19:30 +0200

    untested product function

--- TYPPrototype/TYPPrototype/AddProduct.aspx.cs
+++ TYPPrototype/TYPPrototype/AddProduct.aspx.cs
@@ -9,9 +9,26 @@ namespace TYPPrototype
 {
     public partial class addProduct : System.Web.UI.Page
     {
+        ProductService.ProductServiceClient Pclient;
         protected void Page_Load(object sender, EventArgs e)
         {
+            Pclient = new ProductService.ProductServiceClient();
+        }
 
+        protected void Savebtn_Click(object sender, EventArgs e)
+        {
+            ProductService.Product Pro = new ProductService.Product
+            {
+                P_Name = Pname.Value,
+                P_Price = Int32.Parse(Pprice.Value),
+                P_Image = Pimage.Value,
+                P_Quantity = Int32.Parse(PQuant.Value),
+                P_Type = Ptype.Value,
+                P_Code = PCode.Value,
+                Supplier_Name = SName.Value,
+                W_Name = WName.Value
+            };
+            string result = Pclient.AddProduct(Pro);
         }
     }
 }
